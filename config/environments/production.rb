f95d2a084c017309fb90625fd9fb391c771a0726
James Treanor <jtreanor3@gmail.com>
2013-02-24 18:49:53 +0000

    Set mail host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ SummerCourses::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  #config.action_mailer.default_url_options = { :host => 'summercourses.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'summercourses.herokuapp.com' }
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
