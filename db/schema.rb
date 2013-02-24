5cd5aba7ee3672eca60f7cb2778aeb5b9a4228cb
James Treanor <jtreanor3@gmail.com>
2013-02-24 18:03:25 +0000

    Fixed confirmable bug

--- db/schema.rb
+++ db/schema.rb
@@ -119,6 +119,11 @@ ActiveRecord::Schema.define(:version => 0) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    ## Confirmable
+    t.string   :confirmation_token
+    t.datetime :confirmed_at
+    t.datetime :confirmation_sent_at
+    t.string   :unconfirmed_email # Only if using reconfirmable
   end
 
   add_index "students", ["countryCode"], :name => "countryCode_idx"
