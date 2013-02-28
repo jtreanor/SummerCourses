3506911845d08a4d678794fa65a3372550bd702a
James Treanor <jtreanor3@gmail.com>
2013-02-28 09:47:54 +0000

    Added active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SummerCourses::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   devise_for :students, :controllers => { :regsitrations => 'students/registrations' } 
   resources :categories, only: [:index, :show]
   resources :courses, only: [:show]
