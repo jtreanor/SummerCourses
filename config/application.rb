7a2bdf91d3cdf0033594b218201d173b535d03d5
James Treanor <jtreanor3@gmail.com>
2013-03-01 10:23:44 +0000

    changed precompile

--- config/application.rb
+++ config/application.rb
@@ -60,9 +60,6 @@ module SummerCourses
     config.assets.version = '1.0'
 
     #Added to fix devise/active admin issue ?
-    config.assets.initialize_on_precompile = false
-
-    # Precompile additional assets. Defaults to [application.js, application.css, non-JS/CSS]
-    config.assets.precompile += ['active_admin.css', 'active_admin.js'] 
+    config.assets.initialize_on_precompile = true
   end
 end
