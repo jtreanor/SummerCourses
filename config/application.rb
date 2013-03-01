950f50b0b11578ec32630271d1ac56688bd8345a
James Treanor <jtreanor3@gmail.com>
2013-03-01 19:47:18 +0000

    Trying to fix activeadmin precompile

--- config/application.rb
+++ config/application.rb
@@ -63,6 +63,7 @@ module SummerCourses
     config.assets.initialize_on_precompile = false
 
     # Precompile additional assets. Defaults to [application.js, application.css, non-JS/CSS]
-    config.assets.precompile += ['active_admin.css', 'active_admin.js','print.css'] 
+    #config.assets.precompile += ['active_admin.css', 'active_admin.js','print.css'] 
+    config.assets.compile = true
   end
 end
