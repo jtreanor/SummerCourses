e220cce476dc94d4bfb309812ce6a4d250c8e8c5
James Treanor <jtreanor3@gmail.com>
2013-03-06 15:55:59 +0000

    Removed mailer.

--- app/mailers/message_mailer.rb
+++
@@ -1,9 +0,0 @@
-class MessageMailer < ActionMailer::Base
-	default :from => "message@summercourses.herokuapp.com"
-	 
-	  def message
-	    @url  = "http://summercourses.herokuapp.com/"
-	    mail(:to => "jtreanor3@gmail.com", :subject => "Welcome to My Awesome Site")
-	  end
-	end
-end
