a317c8e9f40a6891d6a4b4e9ac18538bbb582e01
James Treanor <jtreanor3@gmail.com>
2013-03-06 18:03:13 +0000

    Added reply plain.

--- app/controllers/incoming_messages_controller.rb
+++ app/controllers/incoming_messages_controller.rb
@@ -3,11 +3,9 @@ class IncomingMessagesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    Rails.logger.info "ENVELOPE:" + params[:envelope][:to] + '\n\n\n\n' if params[:envelope] # print the to field to the logs
-    Rails.logger.info "SUBJECT:" + params[:subject] + '\n\n\n\n' # print the subject to the logs
-    Rails.logger.info "PLAIN:" + params[:plain] + '\n\n\n\n' # print the decoded body plain to the logs if present
-    Rails.logger.info "HTML:" + params[:html] + '\n\n\n\n' # print the html decoded body to the logs if present
-    Rails.logger.info params[:attachments][0] if params[:attachments] # A tempfile attachment if attachments is populated
+    Rails.logger.info "SUBJECT:" + params[:subject] 
+    Rails.logger.info "PLAIN:" + params[:plain]
+    Rails.logger.info "REPLY PLAIN:" + params[:reply_plain]
 
     # Do some other stuff with the mail message
 
