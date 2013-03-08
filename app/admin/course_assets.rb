19f5210d20810c7a3d4eb18fa953d5e969d628bc
James Treanor <jtreanor3@gmail.com>
2013-03-08 12:26:40 +0000

    Changed course_asset form.

--- app/admin/course_assets.rb
+++ app/admin/course_assets.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register CourseAsset do
-  
+  form :html => { :enctype => "multipart/form-data" } do |f|
+   f.inputs "Details" do
+   	f.input :course
+    f.input :asset
+  end
+  f.buttons
+ end
 end
