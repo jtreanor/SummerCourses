79bd96b37d7e31566d7376ea80946fb41d033494
James Treanor <jtreanor3@gmail.com>
2013-03-12 11:39:43 +0000

    Annotated

--- app/models/refund.rb
+++ app/models/refund.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: refunds
+#
+#  id                      :integer          not null, primary key
+#  refund_transaction_id   :string(45)       not null
+#  original_transaction_id :string(45)       not null
+#
+
 class Refund < ActiveRecord::Base
   # attr_accessible :title, :body
 end
--- test/unit/refund_test.rb
+++ test/unit/refund_test.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: refunds
+#
+#  id                      :integer          not null, primary key
+#  refund_transaction_id   :string(45)       not null
+#  original_transaction_id :string(45)       not null
+#
+
 require 'test_helper'
 
 class RefundTest < ActiveSupport::TestCase
