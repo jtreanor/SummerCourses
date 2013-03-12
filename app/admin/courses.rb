8e62ab4b17b2d55aaaf48e6ccf8821448a0efb35
James Treanor <jtreanor3@gmail.com>
2013-03-12 09:53:43 +0000

    Undid new category support

--- app/admin/courses.rb
+++ app/admin/courses.rb
@@ -5,24 +5,29 @@ ActiveAdmin.register Course do
    	  f.input :brief_description
    	  f.input :description
       f.input :teacher_id
-	  f.input :number_of_places
-	  f.input :price
-	  f.input :deposit
-	  f.input :category
+	    f.input :number_of_places
+	    f.input :price
+	    f.input :deposit
+	    f.input :category
+      #f.inputs "Category", :for => [:category, f.object.category || Category.new] do |meta_form|
+      #  f.input :category, label: "Existing category" 
+      #  meta_form.input :category_name, label: "New Category"
+      #end
     end
 
-	f.inputs "Assets" do
-	  	f.has_many :course_assets do |ca|
-	  		ca.inputs "Existing Asset" do
-	    		ca.input :asset
-	  		end
-        ca.inputs "New Asset", :for => [:asset, Asset.new ] do |fm|
-            fm.input :description
-            fm.input :asset, :as => :file
-        end
-	  	end
-  	end
+  	f.inputs "Assets" do
+  	  	f.has_many :course_assets do |ca|
+  	  		ca.inputs "Existing Asset" do
+  	    		ca.input :asset
+  	  		end
+          ca.inputs "New Asset", :for => [:asset, Asset.new ] do |fm|
+              fm.input :description
+              fm.input :asset, :as => :file
+          end
+  	  	end
+    	end
 
-  	f.buttons
+    f.buttons
   end
+
 end
