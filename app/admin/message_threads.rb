ce3d9f7266d39a3bf7e8370ae5542bb1595e3b0e
James Treanor <jtreanor3@gmail.com>
2013-03-15 17:02:51 +0000

    Table formatting

--- app/admin/message_threads.rb
+++ app/admin/message_threads.rb
@@ -4,15 +4,12 @@ ActiveAdmin.register MessageThread do
 	  	row :id
 	  	row :user_email
 	end
-	div :class => :panel do
-		h3 "Messages"
-		div :class => :panel_contents do
+
 			message_thread.messages.each do |m|
 			div :class => :panel do
 			h3 "Message"
 			div :class => :panel_contents do
 				table :class=> :attributes_table do
-					
 						th("Message Body")
 						td(simple_format(m.content)) 
 					
@@ -20,8 +17,7 @@ ActiveAdmin.register MessageThread do
 			end
 			end
 			end
-		end
-	end
+
   	active_admin_comments
   end
   
