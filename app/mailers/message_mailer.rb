fcfc08afef8f3e6dc08b57068ae6250e30465305
James Treanor <jtreanor3@gmail.com>
2013-03-19 14:04:14 +0000

    Typo

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -4,6 +4,6 @@ class MessageMailer < ActionMailer::Base
   def reply_email(message)
     @message = message
     mail(:to => message.message_thread.user_email, :subject => "#{message.message_thread.subject} #{message.message_thread.id}")
-    logger.info "Send email with subject #{"#{message.message_thread.subject} #{message.message_thread.id}"} to #{message.message_thread.user_email}"
+    logger.info "Sent email with subject #{"#{message.message_thread.subject} #{message.message_thread.id}"} to #{message.message_thread.user_email}"
   end
 end
