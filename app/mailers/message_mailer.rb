883025c710d24425abe9308d8f0aeff2da948613
James Treanor <jtreanor3@gmail.com>
2013-03-20 20:08:31 +0000

    Changed quote order

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -5,7 +5,7 @@ class MessageMailer < ActionMailer::Base
     @message = message
 
     #Get most recent question in the thread.
-    @recent_message = @message.message_thread.messages.find_all{|m| !m.is_response }.sort_by(&:created_at)[0]
+    @recent_message = @message.message_thread.messages.find_all{|m| !m.is_response }.sort_by{|m| -m[:created_at]}[0]
 
     mail(:to => message.message_thread.user_email, :subject => "Re: #{message.message_thread.subject} - #{message.message_thread.id}")
     logger.info "Sent email with subject #{"#{message.message_thread.subject} - #{message.message_thread.id}"} to #{message.message_thread.user_email}"
