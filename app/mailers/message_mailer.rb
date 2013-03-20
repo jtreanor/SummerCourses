57dcec84e9c82b387474ba1893decf6a84402ec0
James Treanor <jtreanor3@gmail.com>
2013-03-20 19:56:15 +0000

    Changed quoting behaviour

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -3,6 +3,10 @@ class MessageMailer < ActionMailer::Base
 
   def reply_email(message)
     @message = message
+
+    #Get most recent question in the thread.
+    @recent_message = @message.message_thread.messages.find_all{|m| !m.is_response }.sort_by(&:created_at)[0]
+
     mail(:to => message.message_thread.user_email, :subject => "Re: #{message.message_thread.subject} - #{message.message_thread.id}")
     logger.info "Sent email with subject #{"#{message.message_thread.subject} - #{message.message_thread.id}"} to #{message.message_thread.user_email}"
   end
