f8dfb411c695c5b44e14359fcaf4b8518e08008c
James Treanor <jtreanor3@gmail.com>
2013-03-21 12:46:11 +0000

    Name in email

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,5 +1,5 @@
 class MessageMailer < ActionMailer::Base
-  default from: "ask@summercourses.mailgun.org"
+  default from: "UCC Summer Course Office <ask@summercourses.mailgun.org>"
 
   def reply_email(message)
     @message = message
