4bc479440e326843f9891223178e7b337a1171b7
James Treanor <jtreanor3@gmail.com>
2013-03-23 14:44:57 +0000

    Changed devise from email.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "admin@summercourses.mailgun.org"
+  config.mailer_sender = "UCC Summer Courses Office <admin@summercourses.mailgun.org>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
