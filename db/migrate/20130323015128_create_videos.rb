216e3676a939f5e3f8d718e345f5118546c95e0a
James Treanor <jtreanor3@gmail.com>
2013-03-24 14:16:51 +0000

    Removed migrations.

--- db/migrate/20130323015128_create_videos.rb
+++
@@ -1,9 +0,0 @@
-class CreateVideos < ActiveRecord::Migration
-  def change
-    create_table :videos do |t|
-      t.text 'url', :null => false
-      t.string 'description', :null => false
-      t.timestamps
-    end
-  end
-end
