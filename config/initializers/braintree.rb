5b8d2d3f3bac86264b3cbe90520decba70137cb6
James Treanor <jtreanor3@gmail.com>
2013-03-31 15:58:33 +0100

    Added braintree sandbox credentials.

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
 Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
-Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
-Braintree::Configuration.private_key = ENV['BRAINTREE_PRIVATE_KEY']
+Braintree::Configuration.merchant_id = '6b6c6smqn4ddbrvy'
+Braintree::Configuration.public_key = 'bkmz9ztnjt6f3jvj'
+Braintree::Configuration.private_key = 'e37569f722592948d8e9e262fec86478'
